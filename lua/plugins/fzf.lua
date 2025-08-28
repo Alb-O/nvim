473568551d97e44b5801842843590b95d23f847a
Albert O'Shea <albertoshea2@gmail.com>
2025-08-28 11:04:55 +1000

    feat: update fzf conf

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -3,20 +3,22 @@ return {
     "ibhagwan/fzf-lua",
     dependencies = { "echasnovski/mini.icons" },
     opts = {
-      'skim', -- use skim instead of fzf
+      'border-fused',
+      hls = {
+        normal = "Normal",
+        preview_normal = "Normal",
+        border = "NonText",
+        preview_border = "NonText",
+      },
+      fzf_bin = 'sk',
       defaults = {
         git_icons = true,
-        winopts = {
-          height = 0.85,
-          width = 0.80,
-          row = 0.35,
-          col = 0.50,
-          border = "none",
-          preview = {
-            border = "none",
-            scrollbar = false,
-            delay = 0,
-          },
+      },
+      winopts = {
+        backdrop = 100,
+        border = "single",
+        preview = {
+          border = "single",
         },
       },
     },
@@ -25,10 +27,12 @@ return {
       require("fzf-lua").setup(opts)
       -- Register fzf-lua as the handler for vim.ui.select
       require("fzf-lua").register_ui_select()
+      -- Keymaps
+      vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<CR>", { desc = "Find files" })
+      vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<CR>", { desc = "Find content (grep)" })
+      vim.keymap.set({ "n", "v", "i" }, "<C-x><C-f>",
+        function() require("fzf-lua").complete_path() end,
+        { silent = true, desc = "Fuzzy complete path" })
     end,
-
-    -- Keymaps
-    vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<CR>", { desc = "Find files" }),
-    vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<CR>", { desc = "Find content (grep)" }),
   },
 }
